eacc7dd54412868ffd68fa6a84d247d9c2850032
AXELLNUGROHOzy12 <fadilahalhabsyi78@gmail.com>
2025-06-05 11:13:40 +0700

    brat.js

--- api/imagecreator/brat.js
+++ api/imagecreator/brat.js
@@ -3,7 +3,7 @@ app.get('/imagecreator/brat', async (req, res) => {
         try {
             const { apikey, text } = req.query
             if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-            const pedo = await getBuffer(`https://brat.caliphdev.com/api/brat?text=${text}`)
+            const pedo = await getBuffer(`https://api.nekorinn.my.id/maker/brat-v2?text=${text}`)
             res.writeHead(200, {
                 'Content-Type': 'image/png',
                 'Content-Length': pedo.length,
