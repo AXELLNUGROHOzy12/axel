121c4004fe4b5760f29da7c3e624f7c2479b298d
AXELLNUGROHOzy12 <fadilahalhabsyi78@gmail.com>
2025-06-06 07:22:51 +0700

    brat.js

--- api/imagecreator/brat.js
+++ api/imagecreator/brat.js
@@ -100,20 +100,3 @@ module.exports = (app) => {
     }
   })
 }
-
-    
-app.get('/imagecreator/bratvideo', async (req, res) => {
-        try {
-            const { apikey, text } = req.query
-            if (!global.apikey.includes(apikey)) return res.json({ status: false, error: 'Apikey invalid' })
-            const pedo = await getBuffer(`https://api.nekorinn.my.id/maker/bratvid?text=${text}`)
-            res.writeHead(200, {
-                'Content-Type': 'video/mp4',
-                'Content-Length': pedo.length,
-            });
-            res.end(pedo);
-        } catch (error) {
-            res.status(500).send(`Error: ${error.message}`);
-        }
-    });    
-}
